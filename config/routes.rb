a7900f17520330803b4cc0218cef8cde8b3785fa
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-03-13 08:29:40 +0800

    User registration with name & email, and input password after email confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmations: 'confirmations'
+  }
+
+  as :user do
+    put '/users/confirmation' => 'confirmations#update', via: :patch, as: :update_user_confirmation
+  end
 end
